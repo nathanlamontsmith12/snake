0906d7fb86297fe67c7f3a1f2cb4ffefef0b8bd5
nathanlamontsmith12 <andinto@gmail.com>
2019-09-12 10:12:07 -0500

    refactored; completely changed movement mechanism; lots of bugs

--- js/app.js
+++ js/app.js
@@ -1,77 +1,94 @@
 console.log("linked up: SNAKE")
 
 class Food {
-    constructor(x, y){
+    constructor(x, y, h=5, w=5){
         this.x = x;
         this.y = y;
-        this.height = 2;
-        this.width = 3;
+        this.height = h;
+        this.width = w;
+        this.point = [x + Math.floor(w/2), y + Math.floor(h/2)]
+        this.eaten = false;
     }
     draw(){
-        game.ctx.fillStyle = "limegreen"
         game.ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+    getCoordinates(){
+        return this.point;
+    }
+}
+
+class Segment {
+    constructor(x, y, w=10, h=10) {
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+    }
+    draw(){
+        game.ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+    getCoordinates(){
+        return([this.x, this.y])
     }
 }
 
 class Snake {
-    constructor(startX=5, startY=5, width=10, height=10, speed=5){
-      this.x = startX;
-      this.y = startY;
-      this.height = height;
-      this.increment = width;
-      this.width = width;
-      this.direction = "r";
-      this.speed = speed; 
-      this.active = false;
-      this.hitbox = {
-          xmin: startX,
-          xmax: startX + width,
-          ymin: startY,
-          ymax: startY + height
-      }
+    constructor(w=10, h=10){
+        this.segments = [];
+        this.headInd = 0;
+        this.direction = "d";
+        this.active = false;
+        this.hasEaten = false;
+        this.width = w;
+        this.height = h;
+    }
+    activate(){
+        for (let i = 1; i <= 5; i++){
+            const newSeg = new Segment(this.width*i, 25, this.width, this.height)
+            this.segments.push(newSeg)
+        }
+        this.active = true;
     }
     draw(){
-       game.ctx.fillStyle = "limegreen"
-       game.ctx.fillRect(this.x, this.y, this.width, this.height)
+        this.segments.forEach(seg => seg.draw())
     }
     move(){
-        if (this.direction == "r") {
-            if (this.x + this.width >= 500) {
-                this.direction = "d"
+        if (this.active){
+            this.addSegment()
+            if (!this.hasEaten){
+                this.removeSegment()
             } else {
-                this.x += this.speed;
+                this.hasEaten = false;
             }
         }
-        if (this.direction == "l") {
-            if (this.x <= 0) {
-                this.direction = "u"
-            } else {
-            this.x -= this.speed;
-            }
-        }
-        if (this.direction == "u") {
-            if (this.y <= 0) {
-                this.direction = "r"
-            } else {
-                this.y -= this.speed;
-            }
-        }
-        if (this.direction == "d") {
-           if (this.y + this.height >= 500) {
-                this.direction = "l"
-           } else {
-                this.y += this.speed;
-           }
-        }
-
-        this.updateHitbox();
     }
-    updateHitbox(){
-        this.hitbox = {
-            xmin: this.x,
-            xmax: this.x + this.width,
-            ymin: this.y,
-            ymax: this.y + this.height
+    addSegment(){
+        let newX = this.segments[this.headInd].x;
+        let newY = this.segments[this.headInd].y; 
+        if (this.direction == "r"){
+            newX += this.width;
+        }
+        if (this.direction == "l"){
+            newX -= this.width;
+        }
+        if (this.direction == "d"){
+            newY += this.width;
+        }
+        if (this.direction == "u"){
+            newY -= this.width;
+        }
+        const newSeg = new Segment(newX, newY, this.width, this.height)
+        if(this.headInd === 0) {
+            this.segments.unshift(newSeg)
+        } else {
+            this.segments.push(newSeg)
+        }
+    }
+    removeSegment(){
+        if (this.headInd === 0) {
+            this.segments.pop();
+        } else {
+            this.segments.shift();
         }
     }
     changeDirection(dir){
@@ -84,44 +101,57 @@ class Snake {
                 return;
         } else {
             this.direction = dir;
+            if (this.direction == "l"){
+                this.headInd = 0;
+            }
+            if (this.direction == "r"){
+                this.headInd = this.segments.length - 1;
+            }
         }
     }
-    check(){
-        if (this.isDead()){
-            game.endGame()
-        };
-        if(this.isEating()){
-            this.eat()
-        };
+    handleNewFrame(){
+        this.move();
+        if (this.checkDeath()){
+            this.active = false;
+        } 
+        if (this.checkEating()){
+            this.hasEaten = true;
+            game.food.eaten = true;
+        }
     }
-    isDead(){
-        
+    checkDeath(){
+        const headLoc = this.segments[this.headInd].getCoordinates();
+       
+        // collision w/ walls: 
+        if (headLoc[0] > game.ctx.canvas.width || headLoc[0] < 0 || 
+            headLoc[1] > game.ctx.canvas.height || headLoc[1] < 0) {
+            return true;
+        }
+
+        // collision w/ self: 
+        for (let i = 0; i < this.segments.length; i++) {
+            if (i === this.headInd) {
+                continue;
+            }
+            const segmentLoc = this.segments[i].getCoordinates();
+            if (headLoc[0] === segmentLoc[0] && headLoc[1] === segmentLoc[1]){
+                return true;
+            }
+        }
+
+        return false;
     }
-    isEating(){
-        const foodRangeX = [game.food.x, game.food.x + game.food.width]
-        const foodRangeY = [game.food.y, game.food.y + game.food.height]
-        if (
-            (this.x < foodRangeX[0] && this.x + this.increment > foodRangeX[1]) &&
-            (this.y < foodRangeY[0] && this.y + this.height > foodRangeY[1])
-            ) {
+    checkEating(){
+        const headLoc = this.segments[this.headInd].getCoordinates();
+        const foodLoc = game.food.getCoordinates();
+        if (headLoc[0] < foodLoc[0] && 
+            headLoc[0] + this.width > foodLoc[0] &&
+            headLoc[1] < foodLoc[0] && 
+            headLoc[1] + this.height > foodLoc[1]) {
                 return true;
             } else {
                 return false;
             }
-    }
-    eat(){
-        game.food = null;
-        this.width += this.increment;
-        game.setFood();
-    }
-}
-
-class Info {
-    constructor(){
-
-    }
-    draw(){
-        console.log("INFO SCREEN DRAW FIRED")
     }
 }
 
@@ -132,45 +162,51 @@ const game = {
     food: null,
     ctx: document.querySelector("canvas").getContext("2d"),
     start(){
+        this.ctx.fillStyle = "limegreen"
         this.setFood();
         this.player = new Snake();
-        this.infoscreen = new Info();
-        this.infoscreen.draw();
+        this.player.activate();
         startAnimation();
     },
     setFood(){
-        const randX = Math.floor(Math.random()*501)
-        const randY = Math.floor(Math.random()*501)
+        const randX = Math.floor(Math.random() * (this.ctx.canvas.width - 50)) + 25;
+        const randY = Math.floor(Math.random() * (this.ctx.canvas.height - 50)) + 25;
         this.food = new Food(randX, randY)
     },
-    wipe(){
-        this.ctx.clearRect(0, 0, 500, 500)
+    clearScreen(){
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height)
+    },
+    nextFrame(){
+        if (this.food.eaten) {
+            this.setFood()
+        } 
+        this.player.handleNewFrame();
     },
     updateDisplay(){
-        this.wipe();
-        if (this.food) {
-            this.food.draw()
-        }
-        this.player.move();
+        this.food.draw();
         this.player.draw();
     },
     endGame(){
-        this.stopAnimation();
+        // do some stuff: 
         console.log("DEAD")
-    },
-    stopAnimation(){
-        cancelAnimationFrame(this.animation)
-        this.animation = null
     }
 }
 
 function startAnimation(){
+    game.clearScreen();
+    game.nextFrame();
     game.updateDisplay();
-    const checkResult = game.player.check();
-    if (checkResult) {
-        game.endGame(checkResult)
+    if (!game.player.active){
+        stopAnimation();
+        game.endGame();
+        return;
     }
     game.animation = window.requestAnimationFrame(startAnimation)
+}
+
+function stopAnimation(){
+    cancelAnimationFrame(game.animation)
+    game.animation = null
 }
 
 document.body.addEventListener("keydown", (evt)=>{
